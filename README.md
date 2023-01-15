cd11eced79a0b15326dd2db798ea663e80df34ea
Gabor A <gabor@derive.ca>
2023-01-15 15:12:58 -0500

    Added install scripts

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dotfiles
+
+### Prerequisites
+* git
+* sudo access for application install
+
+### Installation 
+```bash
+git clone https://github.com/galbitz/dotfiles
+./dotfiles/install.sh
+```
+
+### Inspired by:
+* https://dotfiles.github.io/
+* https://github.com/tomnomnom/dotfiles/blob/master/setup.sh
+* https://victoria.dev/blog/how-to-set-up-a-fresh-ubuntu-desktop-using-only-dotfiles-and-bash-scripts/
+* https://github.com/chrisfarber/dotfiles
---
+++ install.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+dotfilesDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+sudo -n true 2>/dev/null
+cansudo=$?
+
+function add_bash_extra {
+    text_block=$(cat <<EOF
+# dotfile block starts
+source \$HOME/.bashrc_extra
+# dotfile block ends
+
+EOF
+)
+    bashfile="$HOME/.bashrc"
+    if ! grep -qF "$text_block" "$bashfile"; then
+        echo "Adding bash extra to bashfile"
+        echo "$text_block" >> "$bashfile"
+    fi
+}
+
+function install_starship {
+    bindir=$([ "$cansudo" -ne 0 ] && echo --bin-dir $HOME/.local/bin)
+    curl -sS https://starship.rs/install.sh | sh -s --  --force $bindir
+}
+
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${1} ]; then
+    # Existing symlink 
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest} 
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dotfilesDir}/${1} -> ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+function install_applications {
+
+  if [ "$cansudo" -ne 0 ]; then
+    echo "User cannot sudo. Skipping applications"
+    return 0
+  fi
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "MacOS detected"
+    source ${dotfilesDir}/scripts/install_macos.sh
+    return 0
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux detected"
+    if [ -f /etc/os-release ]; then
+      . /etc/os-release
+      OS=$NAME
+      if [[ "$OS" == "Ubuntu" ]]; then
+        echo "Running install for $OS"
+        source ${dotfilesDir}/scripts/install_ubuntu.sh
+      else
+        echo "Don't know how to install apps on $OS"
+      fi
+    else
+      echo "Cannot determine distribution type"
+    fi
+    return 0    
+  fi
+
+  echo "No OS detected. Skipping applications"
+}
+
+function config_git {
+  echo "Running git configuration"
+  source ${dotfilesDir}/scripts/config_git.sh
+}
+
+echo "Dotfile install started."
+
+echo "Installing bash extras"
+add_bash_extra
+
+echo "Installing starship"
+install_starship
+
+echo "Adding symlinks"
+linkDotfile .bashrc_extra
+mkdir -p $HOME/.config
+linkDotfile .config/starship.toml
+
+echo "Configuring git"
+config_git
+
+echo "Install additonal applications"
+install_applications
+
+source ~/.bashrc
+
+echo "Dotfile install finished."
