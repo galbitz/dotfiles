74a2a91271f96ba6017516beb3ce10e9811169d7
Gabor A <gabor@gabor.one>
2024-08-21 11:01:06 -0400

    fix: cosmetic

--- install/terminal/docker-andmore.sh
+++ install/terminal/docker-andmore.sh
@@ -9,6 +9,9 @@ if is_macos; then return 0; fi
 # Exit if already installed
 if [ -f /etc/apt/keyrings/docker.asc ]; then return 0; fi
 
+
+echo "Installing docker"
+
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
@@ -25,6 +28,8 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+echo "Installing podman"
 sudo apt-get -y install podman
 
+echo "Installing distrobox"
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
