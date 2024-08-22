177436fed9b85f19eaa4ec2c85abed55b0786f38
Gabor A <gabor@gabor.one>
2024-08-22 14:13:59 -0400

    fix: yet another prompt

--- install/desktop/1-xserver.sh
+++ install/desktop/1-xserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # This is to avoid some keyboard layout configuration prompt
-sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration
+sudo DEBIAN_FRONTEND=noninteractive apt install -y keyboard-configuration
 sudo apt install -y xserver-xorg-core xserver-xorg-video-all xinit xinput x11-xserver-utils x11-utils xrdp lightdm kitty i3 i3blocks alacritty
