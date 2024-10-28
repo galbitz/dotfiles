c980398f8cffa85bbe3c2f2dacc7d05aedb1a865
Gabor A <gabor@gabor.one>
2024-10-28 15:32:49 -0400

    fix: missing package (hetzner issue)

--- install/desktop/1-xserver.sh
+++ install/desktop/1-xserver.sh
@@ -2,7 +2,7 @@
 
 # This is to avoid some keyboard layout configuration prompt
 sudo DEBIAN_FRONTEND=noninteractive apt install -y keyboard-configuration
-sudo apt install -y xserver-xorg-core xserver-xorg-video-all xinit xinput x11-xserver-utils x11-utils xrdp lightdm kitty i3 i3blocks alacritty
+sudo apt install -y xserver-xorg-core xserver-xorg-video-all xinit xinput x11-xserver-utils x11-utils xrdp xorgxrdp lightdm kitty i3 i3blocks alacritty
 
 # TODO: alacritty is not yet the default as it's not part of brew and debian version is old to handle toml config
 linkDotfile .config/alacritty
