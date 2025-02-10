5eb38ec78cfd56603d055e2e1fafeffd1c2ffa22
Gabor A <gabor@gabor.one>
2025-02-10 11:44:01 -0500

    fix: additional packages for i3lock script (blur_lock)

--- install/desktop/1-xserver.sh
+++ install/desktop/1-xserver.sh
@@ -2,7 +2,12 @@
 
 # This is to avoid some keyboard layout configuration prompt
 sudo DEBIAN_FRONTEND=noninteractive apt install -y keyboard-configuration
-sudo apt install -y xserver-xorg-core xserver-xorg-video-all xinit xinput x11-xserver-utils x11-utils xrdp xorgxrdp lightdm kitty i3 i3blocks alacritty
+
+# full desktop, comment out if gnome already installed
+sudo apt install -y xserver-xorg-core xserver-xorg-video-all xinit xinput x11-xserver-utils x11-utils xrdp xorgxrdp
+
+# shared packages
+sudo apt install -y lightdm i3 kitty i3blocks alacritty scrot imagemagick
 
 # TODO: alacritty is not yet the default as it's not part of brew and debian version is old to handle toml config
 linkDotfile .config/alacritty
