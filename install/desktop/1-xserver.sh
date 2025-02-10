d0f78d40e1939664b45bcb3f36d3b173bc38ba01
Gabor A <gabor@gabor.one>
2025-02-10 13:11:17 -0500

    fix: include volume control package

--- install/desktop/1-xserver.sh
+++ install/desktop/1-xserver.sh
@@ -7,7 +7,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y keyboard-configuration
 sudo apt install -y xserver-xorg-core xserver-xorg-video-all xinit xinput x11-xserver-utils x11-utils xrdp xorgxrdp
 
 # shared packages
-sudo apt install -y lightdm i3 kitty i3blocks alacritty scrot imagemagick
+sudo apt install -y lightdm i3 kitty i3blocks alacritty scrot imagemagick pavucontrol
 
 # TODO: alacritty is not yet the default as it's not part of brew and debian version is old to handle toml config
 linkDotfile .config/alacritty
