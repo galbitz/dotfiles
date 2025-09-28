7ef00cf90828e280c381c11c5d83b6e0e49aeff4
Gabor A <gabor@gabor.one>
2025-09-28 05:55:28 -0400

    fix condition

--- install/shared/tmux.sh
+++ install/shared/tmux.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Installing tmux and plugins"
 mkdir -p $HOME/.config/tmux
 linkDotfile .config/tmux/tmux.conf
 
 mkdir -p $HOME/.config/tmux_plugins
-git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux_plugins/tpm
+if [ ! -f $HOME/.config/tmux_plugins/tpm/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux_plugins/tpm
+fi
