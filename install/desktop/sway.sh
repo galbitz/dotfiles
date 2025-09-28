173aca2c0d34db08db21cbf2e69f26802ed5cc36
Gabor A <gabor@gabor.one>
2025-09-28 07:31:01 -0400

    feat: file browser gui

--- install/desktop/sway.sh
+++ install/desktop/sway.sh
@@ -7,7 +7,10 @@ sudo apt install -y sway swaylock swayidle waybar
 linkDotfile .config/sway
 linkDotfile .config/waybar
 
+# terminal
 sudo apt install -y alacritty kitty
 linkDotfile .config/alacritty
 linkDotfile .config/kitty
 
+# file manager
+sudo apt install -y thunar
