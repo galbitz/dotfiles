36458c6f096bc98dad415ac993b3824596778e65
Gabor A <gabor@gabor.one>
2025-09-28 06:56:12 -0400

    fix: install terminals

--- install/desktop/sway.sh
+++ install/desktop/sway.sh
@@ -7,3 +7,7 @@ sudo apt install -y sway swaylock swayidle waybar
 linkDotfile .config/sway
 linkDotfile .config/waybar
 
+sudo apt install -y alacritty kitty
+linkDotfile .config/alacritty
+linkDotfile .config/kitty
+
