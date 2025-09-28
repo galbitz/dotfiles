788a6943471e0a02beb0b1ffe8fa570e6f901f37
Gabor A <gabor@gabor.one>
2025-09-28 07:27:37 -0400

    feat: install vscode

--- install/desktop/vscode.sh
+++ install/desktop/vscode.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -f /etc/apt/keyrings/packages.microsoft.gpg ]; then return 0; fi
+if [ -f /usr/share/keyrings/packages.microsoft.gpg ]; then return 0; fi
 
 echo "Installing vscode"
 
-sudo apt-get install -y wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
+sudo apt install -y apt-transport-https wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt install -y apt-transport-https
 sudo apt update
-sudo apt install -y code # or code-insiders
+sudo apt install -y code
