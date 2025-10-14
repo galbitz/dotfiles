4d1029551321b8b73c6b2c34402f4ee62f1ee1df
Gabor A <gabor@gabor.one>
2025-10-14 12:39:08 -0400

    fix: force x11 for i3

--- install/optional/i3.sh
+++ install/optional/i3.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing i3"
+
+# force X11 instead of wayland
+sudo sed -i.bak '/#WaylandEnable/c\WaylandEnable=false' /etc/gdm3/daemon.conf
+
 sudo apt install -y i3 i3blocks fonts-noto rofi dunst
