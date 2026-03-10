7d87c8a3dd59cc54548b185c02ebdcc96565201d
Gabor A <gabor@gabor.one>
2026-03-10 15:37:01 -0400

    fix: stow before niri creates default config

--- install-omarchy.sh
+++ install-omarchy.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+source install/omarchy/stow-global-ignore.sh
+source setup-config.sh
+source ~/.bashrc
+
 source install/omarchy/niri.sh
 source install/omarchy/disable-screensaver.sh
 source install/omarchy/set-default-terminal.sh
 source install/omarchy/install-brave.sh
-source install/omarchy/stow-global-ignore.sh
 source install/omarchy/install-sublime.sh
-source setup-config.sh
-source ~/.bashrc
 source install/shared/git.sh
 source install/shared/add-github-keys.sh
 source install/omarchy/install-webapps.sh
